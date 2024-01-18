bed46050c52ed808e32dd61aac9a44fc98a614e8
hydromelvictor <victorvaddely@gmail.com>
2024-01-18 00:41:19 +0000

    update

--- frontend/src/components/Views.js
+++ frontend/src/components/Views.js
@@ -1,44 +1,41 @@
-import React from 'react';
 import '../style/Views.css'
 import { LinkButtuon } from './ui/Button';
 import { Facebook, Instagram, Linkedin } from 'react-bootstrap-icons';
 
 
 
-class Views extends React.Component {
 
-    render() {
-        
-        return (
-            <main className='vwh'>
-                <div className='body-vwh'>
-                    <div id='logo'></div>
-                    <div className='x-larg title'>Dream Africa</div>
-                    <div className='container row'>
-                        <LinkButtuon href='/login' attrs='btn btn-primary' text='Sign In' />
-                        <LinkButtuon href='/register' attrs='btn btn-primary' text='Sign Up' />
-                    </div>
-                    <div className='container row'>
-                        <a href='facebook' className='da-link'>
-                            <Facebook 
-                                size={30}
-                            />
-                        </a>
-                        <a href='instagram' className='da-link'>
-                            <Instagram 
-                                size={30}
-                            />
-                        </a>
-                        <a href='linkedin' className='da-link'>
-                            <Linkedin 
-                                size={30}
-                            />
-                        </a>
-                    </div>
+function Views() {
+    
+    return (
+        <main className='vwh'>
+            <div className='body-vwh'>
+                <div id='logo'></div>
+                <div className='x-larg title'>Dream Africa</div>
+                <div className='container row'>
+                    <LinkButtuon href='/login' attrs='btn btn-primary' text='Sign In' />
+                    <LinkButtuon href='/register' attrs='btn btn-primary' text='Sign Up' />
                 </div>
-            </main>
-        );
-    }
+                <div className='container row'>
+                    <a href='facebook' className='da-link'>
+                        <Facebook 
+                            size={30}
+                        />
+                    </a>
+                    <a href='instagram' className='da-link'>
+                        <Instagram 
+                            size={30}
+                        />
+                    </a>
+                    <a href='linkedin' className='da-link'>
+                        <Linkedin 
+                            size={30}
+                        />
+                    </a>
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default Views
