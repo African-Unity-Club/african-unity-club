432e1450d4cc0b1c17ba0c542d11f387b17c3f1f
hydromelvictor <victorvaddely@gmail.com>
2024-01-20 13:32:49 +0000

    update

--- frontend/src/components/Index.jsx
+++ frontend/src/components/Index.jsx
@@ -24,6 +24,7 @@ import '../style/All.css'
 function Index() {
 
     const username = 'unknown'
+    let online = 'online'
 
     return (
         <main className="index-main">
@@ -37,39 +38,42 @@ function Index() {
                             <span className="online-dot"></span>
                         </span>
                     </div>
-                    <span className="username">{username}</span>
+                    <span className="username">
+                        {username}
+                        <small>{online}</small>
+                    </span>
                 </div>
 
                 <nav className="side-body">
-                    <Link to={`/dashboard/${username}`} className="sidenav-link">
+                    <Link to={`/dashboard/${username}`} className="body-side-nav-link">
                         <GraphDownArrow size={20} />
                         Dashboard
                     </Link>
-                    <Link to={`/profile/${username}`} className="sidenav-link">
+                    <Link to={`/profile/${username}`} className="body-side-nav-link">
                         <PersonGear size={20}/>
                         Profile</Link>
-                    <Link to="/social" className="sidenav-link">
+                    <Link to="/social" className="body-side-nav-link">
                         <Quote size={20}/>
                         Social</Link>
-                    <Link to="/cultures" className="sidenav-link">
+                    <Link to="/cultures" className="body-side-nav-link">
                         <YinYang size={20}/>
                         Cultures</Link>
-                    <Link to="/talents" className="sidenav-link">
+                    <Link to="/talents" className="body-side-nav-link">
                         <Stars size={20}/>
                         Talents</Link>
-                    <Link to="/stories" className="sidenav-link">
+                    <Link to="/stories" className="body-side-nav-link">
                         <BlockquoteLeft size={20}/>
                         Stories</Link>
-                    <Link to="/careers" className="sidenav-link">
+                    <Link to="/careers" className="body-side-nav-link">
                         <Briefcase size={20}/>
                         Careers</Link>
-                    <Link to="/communities" className="sidenav-link">
+                    <Link to="/communities" className="body-side-nav-link">
                         <People size={20}/>
                         Communities</Link>
-                    <Link to="/news" className="sidenav-link">
+                    <Link to="/news" className="body-side-nav-link">
                         <Newspaper size={20}/>
                         News</Link>
-                    <Link to={`/settings/${username}`} className="sidenav-link">
+                    <Link to={`/settings/${username}`} className="body-side-nav-link">
                         <Sliders size={20}/>
                         Settings</Link>
                 </nav>
@@ -83,7 +87,7 @@ function Index() {
 
             </aside>
 
-            <section className='body-content'>
+            <section className='body-container'>
 
                 <header className='header-content-nav-bar'>
 
@@ -91,23 +95,31 @@ function Index() {
 
                         <Link to="/" className="header-link">
                             <span className='logo'></span>
-                            Dream Africa
+                            Alkabulan
                         </Link>
 
                         <div className="header-search">
-                            <input type="text" className="header-search-input" placeholder="Search Dream Africa" />
+                            <input type="text" className="header-search-input" placeholder="Search Alkabulan" />
                             <button className="header-search-button">
-                                <Search size={24} color='aliceblue'/>
+                                <Search size={20} color='aliceblue'/>
                             </button>
                         </div>
 
                     </div>
 
                     <div className="header-footer">
+                        
+                        <div className='header-footer-head'>
+                            <span className='toggle'>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </span>
+                            <h1 className="header-title">{`Dashboard`}</h1>
+                        </div>
+                        
 
-                        <h1 className="header-title">{`Dashboard`}</h1>
-
-                        <nav className="header-nav">
+                        <nav className="header-footer-nav">
 
                             <Link to={`/calendar/${username}`} className="header-link">
                                 <Calendar2Date size={20} />
@@ -133,56 +145,57 @@ function Index() {
                     </div>
 
                 </header>
+                <div className='container'>
 
-                <Outlet />
+                    <Outlet />
 
-                <footer className='footer-nav-bar'>
+                    <footer className='footer-nav-bar'>
 
-                    <div className="footer-body">
+                        <div className="footer-body">
 
-                        <div className="footer-header">
-                            <div className="footer-logo">
-                                <span className="logo"></span>
+                            <div className="footer-header">
+                                <div className="footer-logo"></div>
+                                <span className="footer-title">Alkabulan</span>
                             </div>
-                            <span className="footer-title">Dream Africa</span>
+
+                            <nav className="footer-nav">
+
+                                <Link to="/about" className="footer-link">About</Link>
+                                <Link to="/help" className="footer-link">Help</Link>
+                                <Link to="/contact" className="footer-link">Contact</Link>
+                                <Link to="/feedback" className="footer-link">Feedback</Link>
+                                <Link to="/donate" className="footer-link">Donate</Link>
+                                <Link to="/support" className="footer-link">Support</Link>
+                                <Link to="/advertise" className="footer-link">Advertise</Link>
+                                <Link to="/partners" className="footer-link">Partners</Link>
+                                <Link to="/careers" className="footer-link">Careers</Link>
+                                <Link to="/developers" className="footer-link">Developers</Link>
+                                <Link to="/investors" className="footer-link">Investors</Link>
+                                <Link to="/press" className="footer-link">Press</Link>
+                                <Link to="/legal" className="footer-link">Legal</Link>
+                                <Link to="/policies" className="footer-link">Policies</Link>
+                                <Link to="/cookies" className="footer-link">Cookies</Link>
+                                <Link to="/ads" className="footer-link">Ads</Link>
+                                <Link to="/more" className="footer-link">More</Link>
+
+                            </nav>
+
                         </div>
 
-                        <nav className="footer-nav">
+                        <div className="footer-footer">
 
-                            <Link to="/about" className="footer-link">About</Link>
-                            <Link to="/help" className="footer-link">Help</Link>
-                            <Link to="/contact" className="footer-link">Contact</Link>
-                            <Link to="/feedback" className="footer-link">Feedback</Link>
-                            <Link to="/donate" className="footer-link">Donate</Link>
-                            <Link to="/support" className="footer-link">Support</Link>
-                            <Link to="/advertise" className="footer-link">Advertise</Link>
-                            <Link to="/partners" className="footer-link">Partners</Link>
-                            <Link to="/careers" className="footer-link">Careers</Link>
-                            <Link to="/developers" className="footer-link">Developers</Link>
-                            <Link to="/investors" className="footer-link">Investors</Link>
-                            <Link to="/press" className="footer-link">Press</Link>
-                            <Link to="/legal" className="footer-link">Legal</Link>
-                            <Link to="/policies" className="footer-link">Policies</Link>
-                            <Link to="/cookies" className="footer-link">Cookies</Link>
-                            <Link to="/ads" className="footer-link">Ads</Link>
-                            <Link to="/more" className="footer-link">More</Link>
+                            <span className="footer-copy">© 2024 Dream Africa</span>
 
-                        </nav>
+                            <span className="footer-terms">
+                                <Link to="/terms" className="footer-link">Terms</Link>
+                                <Link to="/privacy" className="footer-link">Privacy</Link>
+                            </span>
 
-                    </div>
+                        </div>
 
-                    <div className="footer-footer">
+                    </footer>
 
-                        <span className="footer-copy">© 2024 Dream Africa</span>
-
-                        <span className="footer-terms">
-                            <Link to="/terms" className="footer-link">Terms</Link>
-                            <Link to="/privacy" className="footer-link">Privacy</Link>
-                        </span>
-
-                    </div>
-
-                </footer>
+                </div>
 
             </section>
 
